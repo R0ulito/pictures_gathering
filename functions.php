854cd3f7344112a9672f4e4f51aeccc61e3b0d7d
R0ulito <roulit77@gmail.com>
2017-05-20 23:15:56 +0200

    Implantation de la fonction de traitement de février

--- functions.php
+++ functions.php
@@ -3,6 +3,15 @@
 // Une s'occupera des mois en 31 jours, une autre les mois en 30 jours, et la dernière les mois de février
 //
 
+function bissextile($annee) {
+    if( (is_int($annee/4) && !is_int($annee/100)) || is_int($annee/400)) {
+        // l'année  est bissextile
+        return 29;
+    } else {
+        // L'année n'est pas bissextile
+        return 28;
+    }
+}
 
 function recupMois30(){
     $format = ".gif";
@@ -75,10 +84,18 @@ function recupMois31(){
 }
 
 function recupFeb () {
+    $feb = bissextile($_POST['an']);
+    $format = ".gif";
+    if($_POST['resolution'] == '4096_blank') {
+        $format = ".jpg";
+
+    }
+    $str = 0;
+    $jour = 1;
     while ($jour <= $feb) {
         if ($jour <= 9){
-            $image = "http://spaceweather.com/images20" .$an. "/" . $str . $jour . $mois . $an . "/hmi". $reso . $format;
-            echo "<br /> jour " . $jour . " du mois " . $mois . " capté par ".$reso."<br />";
+            $image = "http://spaceweather.com/images20" .$_POST['an']. "/" . $str . $jour . $_POST['mois'] . $_POST['an'] . "/hmi". $_POST['resolution'] . $format;
+            echo "<br /> jour " . $jour . " du mois " . $_POST['mois'] . " capté par ".$_POST['resolution']."<br />";
             // $current = file_get_contents($image);
             //$file = $str.$jour.$mois.$an.".gif";
             //file_put_contents($file, $current);
@@ -87,8 +104,8 @@ function recupFeb () {
             $jour++;
 
         } else {
-            $image = "http://spaceweather.com/images20" .$an. "/" .$jour .$mois .$an. "/hmi".$reso. $format;
-            echo "<br /> jour " . $jour . " du mois " . $mois . " capté par ".$reso."<br />";
+            $image = "http://spaceweather.com/images20" .$_POST['an']. "/" . $jour . $_POST['mois'] . $_POST['an'] . "/hmi". $_POST['resolution'] . $format;
+            echo "<br /> jour " . $jour . " du mois " . $_POST['mois'] . " capté par ".$_POST['resolution']."<br />";
             // $current = file_get_contents($image);
             //$file = $jour.$mois.$an.".gif";
             //file_put_contents($file, $current);
